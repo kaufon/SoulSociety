091989de9659fdd291b82dead890fc3dfc8475c2
kaufon <kauanfvale@gmail.com>
2024-06-10 21:11:59 -0300

    🚧in progess: add initial base for contract

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract HelloWorld {
+    string public message = "Hello World";
+
+    function SetMessage(string memory newMessage) public {
+        message = newMessage;
+        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
+        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+
+
+
+    }
+}
